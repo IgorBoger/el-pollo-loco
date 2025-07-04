9f1949ba6a0f0b93e21a15692bb8d34530c70364
Igor Boger <igorboger26@gmail.com>
2025-07-04 14:55:10 +0200

    The character’s left and right movement on the X-axis has been limited in Level1.

--- classes/level.class.js
+++ classes/level.class.js
@@ -6,8 +6,7 @@ class Level {
     altLayers;
     level_end_x = 1 * 720;
 
-
-    constructor(enemies, clouds, backgroundObjects, layers, altLayers ) {
+    constructor(enemies, clouds, backgroundObjects, layers, altLayers) {
         this.enemies = enemies;
         this.clouds = clouds;
         this.backgroundObjects = backgroundObjects;
