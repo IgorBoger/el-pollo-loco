6d6f122d454ad91bbb8d012968a7ce0060741574
Igor Boger <igorboger26@gmail.com>
2025-10-31 07:22:23 +0100

    Added sleep animation; updated walking, hurt, dead, and jump animations.

--- classes/character.class.js
+++ classes/character.class.js
@@ -82,6 +82,17 @@ class Character extends MovableObject {
     prevSpace = false;
     isJumping = false;
 
+    currentAnimation = null; // merkt sich, welche Animation gerade läuft
+    idleTimeoutMs = 15000;              // 15s bis Sleep (Long-Idle)
+    lastActivityAt = performance.now(); // Zeitstempel der letzten Aktivität
+
+    idleFrameMs = 220;   // ruhig atmen
+    sleepFrameMs = 320;  // noch ruhiger
+    walkFrameMs = 80;  // ok für Laufen
+    hurtFrameMs = 140;  // FPS
+    deadFrameMs = 100;  // FPS (optional)
+    lastAnimAt = 0;    // Zeitstempel der letzten Frame-Weiter­schaltung
+
 
     constructor() {
         super().loadImage(this.IMAGES_WALKING[0]);
@@ -123,9 +134,21 @@ class Character extends MovableObject {
                 this.moveLeft();
                 this.otherDirection = true;
             }
-            // if (this.world.keyBaord.SPACE && !this.isAboveGround()) {
-            //     this.jump();
-            // }
+
+            const anyKey =
+                this.world.keyBaord.RIGHT ||
+                this.world.keyBaord.LEFT ||
+                this.world.keyBaord.SPACE ||
+                this.world.keyBaord.UP ||
+                this.world.keyBaord.DOWN ||
+                this.world.keyBaord.THROW;
+
+            const doingSomething =
+                anyKey || this.isAboveGround() || this.isHurt();
+
+            if (doingSomething) {
+                this.lastActivityAt = performance.now();
+            }
 
             const now = performance.now();
             this.handleJumpInput(now);
@@ -135,44 +158,91 @@ class Character extends MovableObject {
 
 
         setInterval(() => {
+            const now = performance.now();
+
             if (this.isDead()) {
-                this.playAnimation(this.IMAGES_DEAD);
+                if (this.currentAnimation !== 'dead') {
+                    this.currentAnimation = 'dead';
+                    this.currentImage = 0;
+                    this.lastAnimAt = 0;
+                }
+                this.maybeAdvance(this.IMAGES_DEAD, now, this.deadFrameMs);
                 this.img = this.imageCache[this.IMAGES_DEAD[this.IMAGES_DEAD.length - 1]];
+
                 if (!this.deadSoundPlayed) {
                     this.world.playEffectSound(this.world.sounds.dead);
                     this.deadSoundPlayed = true;
                 }
+                return;
+            }
 
-            } else if (this.isHurt()) {
-                this.playAnimation(this.IMAGES_HURT);
+            if (this.isHurt()) {
+                if (this.currentAnimation !== 'hurt') {
+                    this.currentAnimation = 'hurt';
+                    this.currentImage = 0;
+                    this.lastAnimAt = 0;
+                }
+                this.maybeAdvance(this.IMAGES_HURT, now, this.hurtFrameMs);
+
                 if (!this.hurtSoundPlayed) {
                     this.world.playEffectSound(this.world.sounds.hurt);
                     this.hurtSoundPlayed = true;
                 }
-            } else if (this.isAboveGround()) {
-                // this.playAnimation(this.IMAGES_JUMPING);
+                return;
+            }
+
+            if (this.isAboveGround()) {
+                this.currentAnimation = 'jump';
                 this.updateJumpAnimation();
                 this.hurtSoundPlayed = false;
+                return;
+            }
+
+            if (this.isJumping) {
+                this.img = this.imageCache[this.IMAGES_JUMPING[0]];
+                this.isJumping = false;
+            }
+
+            if (this.world.keyBaord.RIGHT || this.world.keyBaord.LEFT) {
+                if (this.currentAnimation !== 'walk') {
+                    this.currentAnimation = 'walk';
+                    this.currentImage = 0;
+                    this.lastAnimAt = 0;
+                }
+                this.maybeAdvance(this.IMAGES_WALKING, now, this.walkFrameMs);
+                this.hurtSoundPlayed = false;
             } else {
+                const isSleeping = (now - this.lastActivityAt) >= this.idleTimeoutMs;
 
-                if (this.isJumping) {
-                    // this.img = this.imageCache[this.IMAGES_JUMPING[this.IMAGES_JUMPING.length - 1]];
-                    this.img = this.imageCache[this.IMAGES_JUMPING[0]];
-                    this.isJumping = false;
-                }
-
-                if (this.world.keyBaord.RIGHT || this.world.keyBaord.LEFT) {
-                    this.playAnimation(this.IMAGES_WALKING);
-                    this.hurtSoundPlayed = false;
+                if (isSleeping) {
+                    if (this.currentAnimation !== 'sleep') {
+                        this.currentAnimation = 'sleep';
+                        this.currentImage = 0;
+                        this.lastAnimAt = 0;
+                    }
+                    this.maybeAdvance(this.IMAGES_LONG_IDLE, now, this.sleepFrameMs);
                 } else {
-                    // this.img = this.imageCache[this.IMAGES_JUMPING[this.IMAGES_JUMPING.length - 1]];
-                    this.img = this.imageCache[this.IMAGES_JUMPING[0]];
-                    this.hurtSoundPlayed = false;
+                    if (this.currentAnimation !== 'idle') {
+                        this.currentAnimation = 'idle';
+                        this.currentImage = 0;
+                        this.lastAnimAt = 0;
+                    }
+                    this.maybeAdvance(this.IMAGES_IDLE, now, this.idleFrameMs);
                 }
+                this.hurtSoundPlayed = false;
             }
         }, 50);
+
+
     }
 
+
+    maybeAdvance(images, now, frameMs) {
+        if ((now - this.lastAnimAt) >= frameMs) {
+            this.playAnimation(images);   // erhöht intern currentImage, setzt img
+            this.lastAnimAt = now;
+        }
+    }
 
 
     jump() {
@@ -183,22 +253,27 @@ class Character extends MovableObject {
 
 
     updateJumpAnimation() {
-        const up_fast = 0; 
-        const up_mid = 2; 
-        const up_slow = 3; 
-        const apex = 4; 
-        const down_slow = 5; 
-        const down_mid = 6; 
-        const down_fast = 8; 
+        if (!this.lastJumpFrameAt) this.lastJumpFrameAt = 0;
+        const now = performance.now();
+        if (now - this.lastJumpFrameAt < 120) return; // nur alle 120ms neues Bild
+        this.lastJumpFrameAt = now;
+
+        const up_fast = 0;
+        const up_mid = 2;
+        const up_slow = 3;
+        const apex = 4;
+        const down_slow = 5;
+        const down_mid = 6;
+        const down_fast = 8;
 
         const vy = this.speedY;
         let idx;
 
-        if (vy > 12) idx = up_fast; 
-        else if (vy > 6) idx = up_mid; 
+        if (vy > 12) idx = up_fast;
+        else if (vy > 6) idx = up_mid;
         else if (vy > 2) idx = up_slow;
         else if (vy > -2) idx = apex;
-        else if (vy > -8) idx = down_slow; 
+        else if (vy > -8) idx = down_slow;
         else if (vy > -14) idx = down_mid;
         else idx = down_fast;
 
