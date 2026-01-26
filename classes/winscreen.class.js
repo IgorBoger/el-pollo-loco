563405aa99a6f10b7897fe4788d1dad430a74f69
Igor Boger <igorboger26@gmail.com>
2026-01-26 20:22:02 +0100

    feat: dim background only for winscreen

--- classes/winscreen.class.js
+++ classes/winscreen.class.js
@@ -3,8 +3,8 @@ class Winscreen extends OverlayScreen {
     winScreenConfig = {
         overlayId: 'winOverlay',
         imageSrc: 'img/You won, you lost/You Win A.png',
-        overlayAlpha: 0,
-        overlayColor: 'transparent',
+        overlayAlpha: 0.4,
+        overlayColor: '#000',
         imageAlpha: 1,
         fadeSpeed: 0.06
     };
@@ -22,5 +22,24 @@ class Winscreen extends OverlayScreen {
 
     draw() {
         this.drawWithBaseFrame();
+    }
+
+
+    drawWithBaseFrame() {
+        if (!this.visible) return;
+        this.prepareDraw();
+        this.drawBaseFrame();
+        this.drawDimBackground(this.winScreenConfig.overlayAlpha, this.winScreenConfig.overlayColor);
+        this.drawScreenImage();
+        this.ctx.restore();
+    }
+
+
+    drawDimBackground(alpha, color) {
+        this.ctx.save();
+        this.ctx.globalAlpha = alpha;
+        this.ctx.fillStyle = color;
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+        this.ctx.restore();
     }
 }
